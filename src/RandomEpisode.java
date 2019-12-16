a5ee3caecd77f52ef5ca29b4b9abfd210cfa32f8
millina <milehi@utu.fi>
2019-12-16 18:58:46 +0200

    Generates a random Friends episode to watch.

---
+++ src/RandomEpisode.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.util.Random;
+public class RandomEpisode {
+    public static void main (String[] args) {
+        int[] episodes = {24, 24, 25, 24, 24, 25, 24, 24, 24, 18};
+        Random rnd = new Random();
+        int s = rnd.nextInt(episodes.length)+1;
+        int e = rnd.nextInt(episodes[s-1])+1;
+        System.out.println("Season: "+ s);
+        System.out.println("Episode: "+ e);
+        JOptionPane.showMessageDialog(null, "s:"+s+"e:"+e);
+    }
+}
